00a83531bb53521ce7a5b55a58da81cfa0501520
Chris Atkinson <46910324+chrisatk@users.noreply.github.com>
2023-05-12 10:00:12 -0400

    Create Q2Quiz.py

---
+++ Q2Quiz.py
@@ -0,0 +1,2 @@
+# Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
+# The program will keep score of the number of questions answered correctly.
