678453334ac3c7c696ca4c35174ed6a383ca9ca7
Chris Atkinson <46910324+chrisatk@users.noreply.github.com>
2023-05-12 09:59:19 -0400

    Create Q1Questions.py

---
+++ Q1Questions.py
@@ -0,0 +1,2 @@
+# Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
+# This will be six lines, and then store the questions in the file questions.txt.
